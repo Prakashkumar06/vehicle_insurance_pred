fe45d4323931d22859736115a6c2f365e29d90ac
Prakashkumar06 <prakashkumarsingh213@gmail.com>
2025-07-05 16:48:42 +0530

    project sturcture done and set venv also

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prakash Singh",
+    author_email="prakashkumarsingh213@gmail.com",
+    packages=find_packages()
+)
